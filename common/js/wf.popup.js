893a19b9806a4021256eecb83ac6a7fbe3c68d43
Denis <www_gef_12@mail.ru>
2019-03-26 11:55:04 +0200

    changed popup form

--- common/js/wf.popup.js
+++ common/js/wf.popup.js
@@ -13,6 +13,7 @@ jQuery(document).ready(function($){
     $('.popup-close').on('click', function(){
         $('.popup').fadeOut(500).removeClass('show');
         $('html').removeAttr('style');
+//        $input.removeAttr('checked');
     });
     // Close popup after click not at popup div
     $(document).mouseup(function(e) {
@@ -20,9 +21,9 @@ jQuery(document).ready(function($){
         if (!$('.popup-container').is(e.target) && $('.popup-container').has(e.target).length === 0) {
             $('.popup').fadeOut(500).removeClass('show');
             $('html').removeAttr('style');
+//            $input.removeAttr('checked');
         }
     });
-
 
     /*
     var $card = $('#tariffs .card-content', this);
@@ -33,20 +34,22 @@ jQuery(document).ready(function($){
     console.log($card);
     console.log($popup);
     console.log($a);
-    console.log($input);    
+    console.log($input);
 
-    var $id1 = $input.eq(0).attr('id');
-    var $id2 = $input.eq(1).attr('id');
-    var $id3 = $input.eq(2).attr('id');
+    var $inputId1 = $input.eq(0).attr('id');
+    var $inputId2 = $input.eq(1).attr('id');
+    var $inputId3 = $input.eq(2).attr('id');
+    console.log($inputId1);
+    console.log($inputId2);
+    console.log($inputId3);
 
-    $a.eq(0).attr('id', $id1);
-    $a.eq(1).attr('id', $id2);
-    $a.eq(2).attr('id', $id3);
+    $a.eq(0).attr('id', $inputId1);
+    $a.eq(1).attr('id', $inputId2);
+    $a.eq(2).attr('id', $inputId3);
     console.log($a);
 
     $a.click(function(){
-        $input.eq($(this).index()).attr('checked', 'checked').siblings().removeAttr('checked');
-        $(this).attr('checked', 'checked').siblings().removeAttr('checked');
+        $input.eq($(this).index()).attr('checked', '');
     });
     */
 
